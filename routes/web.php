0d104a2fc3ba2a91cacb04e422096aa2e69c906a
Fathul Muiin <fmuiin14@gmail.com>
2021-01-20 06:33:39 +0700

    cart page

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/categories', 'CategoryController@index')->name('category');
 
 Route::get('/details/{id}', 'DetailController@index')->name('detail');
 
+Route::get('/cart', 'CartController@index')->name('cart');
+
 // Route::get('/', function () {
 //     return view('pages.home');
 // });
