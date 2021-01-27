03469fdd37f78e898c34fdce9baa16a8ee7d01c1
Fathul Muiin <fmuiin14@gmail.com>
2021-01-27 20:53:49 +0700

    7576 datatables and css js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,6 +11,7 @@
     @stack('prepend-style')
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link href="/style/main.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.23/datatables.min.css" />
     @stack('addon-style')
 </head>
 
@@ -23,7 +24,7 @@
                     <img src="/images/admin.png" alt="" class="my-4" style="max-width: 150px;" />
                 </div>
                 <div class="list-group list-group-flush">
-                    <a href="/dashboard.html" class="list-group-item list-group-item-action">Dashboard</a>
+                    <a href="{{route('admin-dashboard')}}" class="list-group-item list-group-item-action">Dashboard</a>
                     <a href="/dashboard-products.html" class="list-group-item list-group-item-action">Products</a>
                     <a href="/dashboard-transactions.html"
                         class="list-group-item list-group-item-action">Categories</a>
@@ -90,8 +91,9 @@
     </div>
     <!-- Bootstrap core JavaScript -->
     @stack('prepend-script')
-    <script src="/vendor/jquery/jquery.slim.min.js"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.23/datatables.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         AOS.init();
