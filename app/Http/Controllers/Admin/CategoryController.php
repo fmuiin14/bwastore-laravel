eb487d5c4f00c7e40383f1dc0e32c1e7ca9c7d5d
Fathul Muiin <fmuiin14@gmail.com>
2021-01-28 23:59:23 +0700

    edit and delete category index

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -106,7 +106,11 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = category::findOrFail($id);
+
+        return view('pages.admin.category.edit', [
+            'item' => $item
+        ]);
     }
 
     /**
@@ -118,7 +122,16 @@ class CategoryController extends Controller
      */
     public function update(CategoryRequest $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->name);
+        $data['photo'] = $request->file('photo')->store('assets/category','public');
+
+        $item = Category::findOrFail($id);
+
+        $item->update($data);
+        
+        return redirect()->route('category.index');
     }
 
     /**
@@ -129,6 +142,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Category::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('category.index');
     }
 }
