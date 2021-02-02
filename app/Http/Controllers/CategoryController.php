115dcbb788a59eb87cb42c2d973b65fec05b59aa
Fathul Muiin <fmuiin14@gmail.com>
2021-02-02 08:15:47 +0700

    821 category page controller and blade

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Category;
+use App\Product;
+
 class CategoryController extends Controller
 {
     public function index()
     {
-    return view('pages.category');
+        $categories = Category::all();
+        $products = Product::with(['galleries'])->paginate(16);
+
+
+    return view('pages.category', [
+        'categories' => $categories,
+        'products' => $products
+    ]);
     }
 }
