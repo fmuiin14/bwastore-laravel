8a1be4b8583c95256f3e4959f820163dd11eab4f
Fathul Muiin <fmuiin14@gmail.com>
2021-02-04 09:27:23 +0700

    831 appzprov\rsp, route home & login dr bawaan laravel ke template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,20 +13,33 @@
                         Belanja kebutuhan utama, <br>
                         menjadi lebih mudah
                     </h2>
-                    <form action="#" class="mt-3">
+                    <form method="POST" action="{{ route('login') }}" class="mt-3">
+                        @csrf
                         <div class="form-group">
                             <label for="email">Email Address</label>
-                            <input type="email" class="form-control w-75">
+                            <input id="email" type="email" class="form-control w-75 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                         </div>
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control w-75">
+                            <input id="password" type="password" class="form-control w-75 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                         </div>
 
-                        <a href="#" class="btn btn-success btn-block w-75 mt-4">
+                        <button type="submit" class="btn btn-success btn-block w-75 mt-4">
                             Sign In to My Account
-                        </a>
-                        <a href="#" class="btn btn-signup btn-block w-75 mt-2">
+                        </button>
+                        <a href="{{route('register')}}" class="btn btn-signup btn-block w-75 mt-2">
                             Sign Up
                         </a>
                     </form>
