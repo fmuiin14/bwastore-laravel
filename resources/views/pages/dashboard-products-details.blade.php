1e22734c242e7511f4a068c170c4dc3e3c671ebf
Fathul Muiin <fmuiin14@gmail.com>
2021-02-09 00:38:56 +0700

    8104 integrasi detail dan edit product

--- resources/views/pages/dashboard-products-details.blade.php
+++ resources/views/pages/dashboard-products-details.blade.php
@@ -17,7 +17,20 @@ Store Dashboard Product Detail
         <div class="dashboard-content">
             <div class="row">
                 <div class="col-12">
-                    <form action="">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>
+                                {{$error}}
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+                    <form action="{{route('dashboard-product-update', $product->id)}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" name="users_id" value="{{Auth::user()->id}}">
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
@@ -25,23 +38,33 @@ Store Dashboard Product Detail
                                         <div class="form-group">
                                             <label for="name">Product Name</label>
                                             <input type="text" class="form-control" id="name" aria-describedby="name"
-                                                name="storeName" value="Papel La Casa" />
+                                                name="name" value="{{$product->name}}" />
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="price">Price</label>
                                             <input type="number" class="form-control" id="price"
-                                                aria-describedby="price" name="price" value="200" />
+                                                aria-describedby="price" name="price" value="{{$product->price}}" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="kategori">Kategori</label>
+                                            <select name="categories_id" class="form-control">
+                                                <option value="{{$product->categories_id}}">Tidak diganti ({{$product->category->name}})</option>
+                                                @foreach ($categories as $category)
+                                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label for="description">Description</label>
-                                            <textarea name="description" id="description" cols="30" rows="4"
-                                                class="form-control">
-The Nike Air Max 720 SE goes bigger than ever before with Nike's tallest Air unit yet for unimaginable, all-day comfort. There's super breathable fabrics on the upper, while colours add a modern edge. Bring the past into the future with the Nike Air Max 2090, a bold look inspired by the DNA of the iconic Air Max 90. Brand-new Nike Air cushioning
-                                </textarea>
+                                            <textarea name="description" id="description" class="form-control">
+                                                {!! $product->description !!}
+                                            </textarea>
                                         </div>
                                     </div>
                                     <div class="col">
@@ -60,35 +83,26 @@ The Nike Air Max 720 SE goes bigger than ever before with Nike's tallest Air uni
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-4">
-                                    <div class="gallery-container">
-                                        <img src="/images/product-card-1.png" alt="" class="w-100" />
-                                        <a class="delete-gallery" href="#">
-                                            <img src="/images/icon-delete.svg" alt="" />
-                                        </a>
+                                @foreach ($product->galleries as $gallery)
+                                    <div class="col-md-4">
+                                        <div class="gallery-container">
+                                            <img src="{{Storage::url($gallery->photos ?? '')}}" alt="" class="w-100" />
+                                            <a class="delete-gallery" href="{{route('dashboard-product-gallery-delete', $gallery->id)}}">
+                                                <img src="/images/icon-delete.svg" alt="" />
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="gallery-container">
-                                        <img src="/images/product-card-2.png" alt="" class="w-100" />
-                                        <a class="delete-gallery" href="#">
-                                            <img src="/images/icon-delete.svg" alt="" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="gallery-container">
-                                        <img src="/images/product-card-3.png" alt="" class="w-100" />
-                                        <a class="delete-gallery" href="#">
-                                            <img src="/images/icon-delete.svg" alt="" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="col mt-3">
-                                    <input type="file" id="file" style="display: none;" multiple />
-                                    <button class="btn btn-secondary btn-block" onclick="thisFileUpload();">
-                                        Add Photo
-                                    </button>
+                                @endforeach
+                                
+                                <div class="col-12 mt-3">
+                                    <form action="{{route('dashboard-product-gallery-upload')}}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <input type="hidden" name="products_id" value="{{$product->id}}">
+                                        <input type="file" name="photos" id="file" style="display: none;" onchange="form.submit()" />
+                                        <button type="button" class="btn btn-secondary btn-block" onclick="thisFileUpload();">
+                                            Add Photo
+                                        </button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
