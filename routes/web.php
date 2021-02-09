c1cfa238484da6c0f24c4a298d4dc43380ddadd1
Fathul Muiin <fmuiin14@gmail.com>
2021-02-09 09:12:36 +0700

    8113 dashboard settings dan account integrasi view, controller, route

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,8 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('/dashboard/settings', 'DashboardSettingController@store')->name('dashboard-settings-store');
 
     Route::get('/dashboard/account', 'DashboardSettingController@account')->name('dashboard-settings-account');
+
+    Route::post('/dashboard/account/{redirect}', 'DashboardSettingController@update')->name('dashboard-settings-redirect');
 });
 
 
